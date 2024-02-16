5dcbc81f1ddc453dac221ae72828324d041e367e
Sujit Sarkar <sujit_s@pursuitsoftware.biz>
2024-02-16 17:50:49 -0500

    removed chroma

--- app.py
+++ app.py
@@ -10,35 +10,36 @@ from flask import Flask, render_template, request
 
 openai_api_key=os.environ['OPENAI_API_KEY']
 
-env_vectordb_create= os.environ["VECTORDB_CREATE"]  # False
+# env_vectordb_create= os.environ["VECTORDB_CREATE"]  # False
 
 
 
-#Persist
-# initialize client, setting path to save data
-db = chromadb.PersistentClient(path="./chroma_db")
-# create collection
-chroma_collection = db.get_or_create_collection("knowledge")
-# assign chroma as the vector_store to the context
-vector_store=ChromaVectorStore(chroma_collection=chroma_collection)
-storage_context=StorageContext.from_defaults(vector_store=vector_store)
+# #Persist
+# # initialize client, setting path to save data
+# db = chromadb.PersistentClient(path="./chroma_db")
+# # create collection
+# chroma_collection = db.get_or_create_collection("knowledge")
+# # assign chroma as the vector_store to the context
+# vector_store=ChromaVectorStore(chroma_collection=chroma_collection)
+# storage_context=StorageContext.from_defaults(vector_store=vector_store)
 
 
-print(f"Args , {env_vectordb_create}!")
-if (env_vectordb_create == "True"):
-    ##Setup
-    print(f"First time , {env_vectordb_create}!")
-    docs=SimpleDirectoryReader("pdfdir").load_data()
-    #Create index and use the storage context above to change default vector store
-    index=VectorStoreIndex.from_documents(docs,storage_context=storage_context,show_progress=True)
+# print(f"Args , {env_vectordb_create}!")
+# if (env_vectordb_create == "True"):
+#     ##Setup
+#     print(f"First time , {env_vectordb_create}!")
+#     docs=SimpleDirectoryReader("pdfdir").load_data()
+#     #Create index and use the storage context above to change default vector store
+#     index=VectorStoreIndex.from_documents(docs,storage_context=storage_context,show_progress=True)
  
-else:
-    print("Not first time", {env_vectordb_create})
-    # load your index from stored vectors
-    index = VectorStoreIndex.from_vector_store( vector_store, storage_context=storage_context)
+# else:
+#     print("Not first time", {env_vectordb_create})
+#     # load your index from stored vectors
+#     index = VectorStoreIndex.from_vector_store( vector_store, storage_context=storage_context)
 
 
-
+docs=SimpleDirectoryReader("pdfdir").load_data()
+index=VectorStoreIndex.from_documents(docs)
 #Function to query
 def query_kb(index_i,query_str):
     """Search Knowledge Base or KB"""
